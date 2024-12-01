ccad4f49dd433b3e6db663584be8373ced717bf3
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-12-02 01:01:11 +0530

    Initial commit

---
+++ 1 Day/script.js
@@ -0,0 +1,11 @@
+ //console.log("Hello");
+ console.log('World');
+ console.log(`Hi`);
+
+ //window.alert("Hi");
+ /*
+ window.alert("Hi");
+ window.alert("Hi");
+ */
+document.getElementById("myh1").textContent=`Hello World`;
+document.getElementById("myp").textContent=`P tag 1`;
