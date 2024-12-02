591abfe374a673be92b127d6476a2e101e08cad1
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2024-12-02 18:54:20 +0530

    js from beginning

---
+++ 2 Day/script.js
@@ -0,0 +1,27 @@
+//Variables
+
+//number
+let quantiy = 10;
+let price = 25.89;
+console.log(`Quantity = ${quantiy}`);
+console.log(`Price = ${price}`);
+console.log(typeof price);
+//string
+let name1 = `Moumoni`;
+let name2 = `Tanmoy`;
+console.log(`Name = ${name1}`);
+console.log(`Name = ${name2}`);
+console.log(typeof name1);
+//boolean
+let x = true;
+let y = false;
+console.log(`x is = ${x}`);
+console.log(`y is = ${y}`);
+console.log(typeof x);
+
+let product = `Cake`;
+let cost = 20.99;
+let hasEgg = true;
+document.getElementById("p1").textContent=`It is ${product}`;
+document.getElementById("p2").textContent=`Cost is ${cost}`;
+document.getElementById("p3").textContent=`Has eggs ${hasEgg}`;
---
+++ 2 Day/script2.js
@@ -0,0 +1,49 @@
+//Arithmatic operators
+
+let x = 32;
+x = x + 5;
+console.log(`Addition = ${x}`);
+x += 5;//-------------> Augmented assignment operator
+console.log(`Addition = ${x}\n`);
+
+let y = 32;
+y = y - 5; 
+console.log(`Subtraction = ${y}`);
+y -= 5;
+console.log(`Subtraction = ${y}\n`);
+
+let m = 32;
+m = m * 2;
+console.log(`Multiply = ${m}`);
+m *= 2;
+console.log(`Multiply = ${m}\n`);
+
+let d = 50;
+d = d / 5;
+console.log(`Division = ${d}`);
+d /= 5;
+console.log(`Division = ${d} \n`);
+
+let e = 5;
+e = e ** 2;
+console.log(`Exponent = ${e}`);
+e **= 2;
+console.log(`Exponent = ${e}\n`);
+
+let z = 39;
+z = z % 5;
+console.log(`Remainder = ${z}`);
+z %= 3;
+console.log(`Remainder = ${z}\n`);
+
+let a = 10;
+a++;//increment by 1
+console.log(a);
+a--;//decrement by 1
+console.log(a);
+
+let i = 1+6*3+16;
+console.log(i); 
+
+let j = 12%5+8/2;
+console.log(j);
