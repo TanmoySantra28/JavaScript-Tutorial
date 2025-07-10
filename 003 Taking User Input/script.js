12bb2a7e7d34c551d281e09a583a2da05d2ab2a0
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2025-07-10 23:36:27 +0530

    Switch to prompt-based user input in script.js
    Replaced the DOM-based input and event handling with prompt-based user input for username and name. The previous DOM code is now commented out, and prompt/console methods are used instead.

--- 003 Taking User Input/script.js
+++ 003 Taking User Input/script.js
@@ -1,14 +1,14 @@
 //Taking user input
 
-// let uname = window.prompt("Enter username = ");
-// let name1;
-// name1 = window.prompt("Enter name = ");
-// console.log(uname);
-// console.log(name1);
+let uname = window.prompt("Enter username = ");
+let name1;
+name1 = window.prompt("Enter name = ");
+console.log(uname);
+console.log(name1);
 
-let username;
-document.getElementById("mysubmit").onclick = function ()
-{
-    username = document.getElementById("mytext").value;
-    document.getElementById("myh1").textContent =`Welcome ${username}`;
-}
+// let username;
+// document.getElementById("mysubmit").onclick = function ()
+// {
+//     username = document.getElementById("mytext").value;
+//     document.getElementById("myh1").textContent =`Welcome ${username}`;
+// }
